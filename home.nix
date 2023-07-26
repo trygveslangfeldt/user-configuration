c66bd546c410f08a3bcf22c88a1f0cf38a34bb00
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-07-26 19:31:07 +0200

    u(home): add pkgs.which

--- home.nix
+++ home.nix
@@ -19,6 +19,7 @@
   # environment.
   home.packages = [
     pkgs.coreutils
+    pkgs.which
     pkgs.liberation_ttf
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
