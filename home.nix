d55362fa64158251b9d8a89f8f0894b4ecc228d0
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-07-26 19:41:32 +0200

    u(home): add pkgs.ripgrep

--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@ in
     pkgs.coreutils
     pkgs.which
     pkgs.liberation_ttf
+    pkgs.ripgrep
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
