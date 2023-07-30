d241c69d3b00501cf5d815775ef3ac649ee4ebd0
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-07-30 16:11:18 +0200

    u(i3status-rust): move into separate file
    additionals:
    - clean up
    - remove volume
    - add icons

---
+++ modules/programs/i3status-rust.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+let
+
+in {
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      custom = {
+        blocks = [
+          {
+            block = "cpu";
+            interval = 1;
+            # format = " $icon $barchart ";
+          }
+          {
+            block = "memory";
+            format = " $icon $mem_used_percents ";
+            warning_mem = 70;
+            critical_mem = 90;
+          }
+          {
+            block = "disk_space";
+            path = "/";
+            info_type = "free";
+            interval = 60;
+            warning = 10.0;
+            alert = 6.0;
+            format = " $path - $available ";
+          }
+          {
+            block = "disk_space";
+            path = "/home";
+            info_type = "free";
+            interval = 60;
+            warning = 10.0;
+            alert = 6.0;
+            format = " $path - $available ";
+          }
+          {
+            block = "battery";
+            device = "BAT0";
+            model = "DELL M59JH18";
+            full_format = " $icon 100% ";
+          }
+          {
+            block = "time";
+            interval = 5;
+            format = " $timestamp.datetime(f:'%a %d/%m/%y %H:%M:%S') ";
+          }
+        ];
+
+        icons = "awesome5";
+        theme = "modern";
+      };
+    };
+  };
+}
--- modules/services/window-managers/i3.nix
+++ modules/services/window-managers/i3.nix
@@ -22,11 +22,11 @@ let
 in {
   imports = [
     ./../../programs/rofi.nix
+    ./../../programs/i3status-rust.nix
   ];
 
   home.packages = [
     pkgs.scrot
-    pkgs.i3status-rust
     pkgs.xxkb
     pkgs.variety
 
@@ -35,90 +35,6 @@ in {
     (pkgs.writeShellScriptBin "i3-session-target" ''
     '')
   ];
-
-  programs.i3status-rust = {
-    enable = true;
-    bars = {
-      custom = {
-        blocks = [
-          {
-            block = "cpu";
-            interval = 1;
-            # format = " $icon $barchart ";
-          }
-          {
-            block = "memory";
-            format = " $icon $mem_used_percents ";
-            warning_mem = 70;
-            critical_mem = 90;
-          }
-          {
-            block = "disk_space";
-            path = "/";
-            info_type = "free";
-            interval = 60;
-            warning = 10.0;
-            alert = 6.0;
-            format = " $path - $available ";
-          }
-          {
-            block = "disk_space";
-            path = "/home";
-            info_type = "free";
-            interval = 60;
-            warning = 10.0;
-            alert = 6.0;
-            format = " $path - $available ";
-          }
-          # {
-            # block = "net";
-            # device = "wlan0";
-            # interval = 5;
-          # }
-          {
-            block = "sound";
-            click = [{
-              button = "left";
-              cmd = "pavucontrol";
-            }];
-          }
-          # {
-            # block = "music";
-            # player = "spotify";
-            # buttons = [ "play" "prev" "next" ];
-            # click = [
-              # {
-                # button = "play";
-                # action = "music_play";
-              # }
-              # {
-                # button = "prev";
-                # action = "music_prev";
-              # }
-              # {
-                # button = "next";
-                # action = "music_next";
-              # }
-            # ];
-          # }
-          {
-            block = "battery";
-            device = "BAT0";
-            model = "DELL M59JH18";
-            full_format = " $icon 100% ";
-          }
-          {
-            block = "time";
-            interval = 5;
-            format = " $timestamp.datetime(f:'%a %d/%m/%y %H:%M:%S') ";
-          }
-        ];
-
-        # icons = "awesome5";
-        theme = "modern";
-      };
-    };
-  };
 
   xsession = {
     enable = true;
