e7c1c016ef7b8ca1453bfe702e7d459d90fcdf71
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-07-31 22:35:21 +0200

    wip

---
+++ modules/services/window-managers/workspace-config.nix
@@ -0,0 +1,68 @@
+{ lib, displays, ... } :
+let
+  setDisplay = { workspaces, display }:
+  let
+    ws = builtins.map (item: item // {
+      value = item.value // {
+        output = display;
+      };
+    }) workspaces;
+  in
+    ws;
+
+  names = [
+    "1:chromium"
+    "2:console"
+    "3:gvim"
+    "4:gvim"
+    "5:console"
+    "6:message"
+    "7:chromium"
+    "8:console"
+    "9:ssh"
+    "10:email"
+    "11:share"
+    "12:share"
+  ];
+
+  templates = builtins.genList (i: {
+    name = "ws" + (toString (i + 1));
+    value = {
+      name = builtins.elemAt names i;
+      output = "";
+    };
+  }) (builtins.length names);
+
+  displayCount = builtins.length displays;
+in
+  builtins.listToAttrs (
+    if displayCount == 1 then
+      setDisplay {
+        workspaces = templates;
+        display = builtins.elemAt displays 0;
+      }
+    else if displayCount == 2 then
+      (setDisplay {
+        workspaces = lib.lists.sublist 0 3 templates;
+        display = builtins.elemAt displays 0;
+      }) ++
+      (setDisplay {
+        workspaces = lib.lists.sublist 3 9 templates;
+        display = builtins.elemAt displays 1;
+      })
+    else if displayCount == 3 then
+      (setDisplay {
+        workspaces = lib.lists.sublist 0 3 templates;
+        display = builtins.elemAt displays 0;
+      }) ++
+      (setDisplay {
+        workspaces = lib.lists.sublist 3 5 templates;
+        display = builtins.elemAt displays 1;
+      }) ++
+      (setDisplay {
+        workspaces = lib.lists.sublist 5 7 templates;
+        display = builtins.elemAt displays 2;
+      })
+    else
+      []
+  )
