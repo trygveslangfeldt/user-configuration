bdc0c53a72e4044c4cd2e78c3f7029f40277d7e4
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-08-07 10:20:59 +0200

    u(home): add pax-utils (lddtree)

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@ in
     pkgs.ripgrep
     pkgs.bat
     pkgs.hostname
+    pkgs.pax-utils
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
