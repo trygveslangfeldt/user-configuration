488e5e73863a6b8e5324765cb736658ba31f3182
Trygve Langfeldt <trygve.sigurdsson.langfeldt@remarkable.no>
2024-04-19 18:43:24 +0200

    feat(darwin): update darwin configuration
    With custom dev and alacritty-tmux

--- modules/targets/darwin/home.nix
+++ modules/targets/darwin/home.nix
@@ -5,17 +5,19 @@ in {
   home.packages = [
     (pkgs.writeShellScriptBin "home-manager-switch" ''
       darwin-rebuild switch \
-        --flake ~/code/github/user-configuration#${configuration}
+        --flake ~/code/github/user-configuration#${configuration} "@"
     '')
     (pkgs.writeShellScriptBin "home-manager-build" ''
       darwin-rebuild build \
-        --flake ~/code/github/user-configuration#${configuration}
+        --flake ~/code/github/user-configuration#${configuration} "@"
     '')
   ];
 
   imports = [
     ../common.nix
+    ./dev.nix
     ../../programs/zsh.nix
+    ../../programs/alacritty-tmux.nix
     ../../programs/vim/neovim.nix
     ../../programs/messenger.nix
   ];
