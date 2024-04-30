3beaec57ff225a836cde131103ab792e678e2cd7
Trygve Langfeldt <trygve.sigurdsson.langfeldt@remarkable.no>
2024-04-30 09:21:08 +0200

    fix: add wezterm mappings

--- modules/programs/wezterm/wezterm.nix
+++ modules/programs/wezterm/wezterm.nix
@@ -1,11 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = [
+    pkgs.wezterm
+  ];
+
+  programs.wezterm = {
+    enable = true;
+  };
   xdg.configFile = {
     "wezterm/wezterm.lua".source = ./wezterm.lua;
   };
 
   home.sessionVariables = {
-    TERMINAL = "wezterm";
+    #TERMINAL = "wezterm";
   };
 }
